e104f77a310b9c8af9a0dbc60167830470d290e9
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-01 13:36:45 +0000

    Added code to fetch ISS flyover times and print out the next spotting times based on your IP address geolocation. NOTE: ISS Pass Predictions API has been removed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIp } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 /* My attempt
 const print = function(ip) {
@@ -12,7 +12,7 @@ const print = function(ip) {
 fetchMyIP(print);
 */
 
-/* Manual test
+/* Manual test for fetching IP
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -23,7 +23,7 @@ fetchMyIP((error, ip) => {
 });
 */
 
-/* Manual test
+/* Manual test for fetching geolocation coordinates
 fetchCoordsByIp("99.232.127.158", (error, coords) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -33,3 +33,44 @@ fetchCoordsByIp("99.232.127.158", (error, coords) => {
   console.log('It worked! Returned coordinates:' , coords);
 });
 */
+
+/* Manual test for fetching ISS flyover times
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+*/
+
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+ const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
